c89bb580c59ab0c134c8dcca7c98c1614c98a47a
Evhen Kulbachenko <45nalitay45@gmail.com>
2024-07-31 12:19:20 +0300

    clorSwitcher is finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timeId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnclick);
+refs.stopBtn.addEventListener('click', onStopBtnclick);
+
+function onStartBtnclick() {
+  timeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+function onStopBtnclick() {
+  clearInterval(timeId);
+  refs.startBtn.disabled = false;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
